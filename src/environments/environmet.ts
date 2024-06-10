9e50419e5d4c2ec5d54475a75269bf978451b4f7
BenjaminQuinteros7 <belenpinito@gmail.com>
2024-06-10 08:29:45 -0300

    firebase

---
+++ src/environments/environmet.ts
@@ -0,0 +1,13 @@
+export const environmet = {
+    productions: false,
+
+    //sacamos la palabra "const" y el ";" y cambiamos el "=" por ":"
+    firebaseConfig: {
+        apiKey: "AIzaSyBf1j2bz7JYH-Pr3E3ug2FkaS1k1t_wQSk",
+        authDomain: "proyecto1-2024-600ff.firebaseapp.com",
+        projectId: "proyecto1-2024-600ff",
+        storageBucket: "proyecto1-2024-600ff.appspot.com",
+        messagingSenderId: "508966983135",
+        appId: "1:508966983135:web:5e261178c34deeed4b606a"
+    }
+};
