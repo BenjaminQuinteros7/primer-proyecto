c8539b666a15a881fbd43fb2411e2c13142c1805
Franco-dr-hd <derenzisjeanfranco@gmail.com>
2024-09-03 11:02:16 -0300

    creamos las directivas @Input y @Output; conectamos la información entre el componente padre e hijo; mostramos la información en un carrusel y una notificación

--- src/app/modules/producto/components/card/card.component.ts
+++ src/app/modules/producto/components/card/card.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { CrudService } from 'src/app/modules/admin/services/crud.service';
-
 
 @Component({
   selector: 'app-card',
@@ -14,8 +13,14 @@ export class CardComponent {
 
   //variable local para seleccionar un producto especifíco
   productoSeleccionado!: Producto;
-
+  //variable local para manejar el estado de un modal
   modalVisible: boolean = false;
+  //booleano para manejar la visibilidad de "última compra"
+  compraVisible: boolean = false;
+
+  //directivas para comunicarse con el componente padre
+  @Input() productoReciente: string = '';
+  @Output() productoAgregado = new EventEmitter<Producto>(); //@Output será definido como un nuevo evento
 
   constructor(public servicioCrud: CrudService) { }
 
@@ -29,8 +34,12 @@ export class CardComponent {
   mostrarVer(info: Producto) {
     //cambio estado del modal a true (ahora es visible)
     this.modalVisible = true;
-
     //guardo en variable seleccionado la información de producto elegido
     this.productoSeleccionado = info;
+  }
+
+  agregarProducto(info: Producto) {
+    this.productoAgregado.emit(info); //enviamos información del producto agregado
+    this.compraVisible = true;
   }
 }
--- src/app/modules/producto/pages/producto/producto.component.ts
+++ src/app/modules/producto/pages/producto/producto.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { Producto } from 'src/app/models/producto';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
+  //string que modificará el valor de @Input en el componente hijo
+  product: string = '';
+  //colección de productos añadidos a la lista
+  productosCarrusel: Producto[] = [];
 
+  productoAnadido(producto: Producto) {
+    //reemplazamos el valor de product
+    this.product = `${producto.nombre} : $${producto.precio}`;
+
+    try {//agregamos la información recibida por el parámetro de la función a la colección del carrusel 
+      this.productosCarrusel.push(producto);
+
+      Swal.fire({
+        title: 'muy bien',
+        text: 'bien se añado tu producto con exito',
+        icon: 'info'
+      })
+    }
+    catch (error) {
+      Swal.fire({
+        title: 'oh nooooooooooouuuuuuuuuuuu',
+        text: 'ehhh error\n' + error,
+        icon: 'error'
+      })
+    }
+  }
 }
